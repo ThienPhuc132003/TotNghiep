a75ddc626f22354eecaafcae2b9d069d31cda847
Trịnh Văn Thiên Phúc <127500862+ThienPhuc132003@users.noreply.github.com>
2025-01-10 02:00:41 +0700

    Update Login.jsx

--- src/pages/User/Login.jsx
+++ src/pages/User/Login.jsx
@@ -54,7 +54,7 @@ const LoginPage = () => {
           });
           if (responseGetProfile.success === true) {
             dispatch(setUserProfile(responseGetProfile.data));
-            console.log("Login successful:", responseGetProfile.data);
+            console.log("dispath thanh cong tét", responseGetProfile.data);
           }
         } catch (error) {
           console.error("Login failed:", error);
@@ -68,10 +68,38 @@ const LoginPage = () => {
     }
   };
 
+  const handleMicrosoftLogin = async () => {
+    try {
+      // Gọi API để lấy URL xác thực Microsoft
+      const response = await Api({
+        endpoint: "user/auth/get-uri-microsoft",
+        method: METHOD_TYPE.GET,
+      });
+      const authUrl = response.data.authUrl;
+
+      if (authUrl) {
+        // Chuyển hướng người dùng tới Microsoft login
+        window.location.href = authUrl;
+      } else {
+        console.error("Microsoft Auth URL not found.");
+      }
+    } catch (error) {
+      console.error("Error fetching Microsoft Auth URL:", error);
+    }
+  };
+
   return (
     <LoginLayout>
       <div className="login-form">
         <h1>Login</h1>
+        <div className="social-login">
+          <button
+            onClick={handleMicrosoftLogin}
+            className="microsoft-login-button"
+          >
+            Login with Microsoft
+          </button>
+        </div>
         <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="emailOrPhoneNumber">Email or Phone Number</label>
@@ -114,6 +142,7 @@ const LoginPage = () => {
             <Link to="/forgot-password">Forgot Password?</Link>
           </p>
         </div>
+       
       </div>
     </LoginLayout>
   );
