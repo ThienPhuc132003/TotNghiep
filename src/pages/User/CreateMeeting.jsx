97cacf9092bc014b1a9930cef6d621b84483a819
ThienPhuc132003 <doremonkuntp132003@gmail.com>
2025-01-20 15:11:15 +0700

    test4

--- src/pages/User/CreateMeeting.jsx
+++ src/pages/User/CreateMeeting.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Api from "../network/Api";
+import Api from "../../network/Api";
 import PropTypes from "prop-types";
 import { METHOD_TYPE } from "../../network/methodType";
 
